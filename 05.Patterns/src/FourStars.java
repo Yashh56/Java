9255bbe50ed7f2b130c6f9fc16a8ef482b1ecfb7
Yash <yashsaini18166@gmail.com>
2024-01-04 12:46:45 +0530

    first Commit

---
+++ 05.Patterns/src/FourStars.java
@@ -0,0 +1,10 @@
+public class FourStars{
+    public static void main(String[] args) {
+        for(int i=0; i<4; i++){
+            for(int j=0;j<4;j++){
+                System.out.println("*");
+            }
+            System.err.println();
+        } 
+    }
+}
