e4529ede2cedfe7962becfe73e362b94e38fbc1a
Yash <yashsaini18166@gmail.com>
2024-01-05 19:23:12 +0530

    Update commit

--- 05.Patterns/src/App.java
+++ 05.Patterns/src/App.java
@@ -1,5 +1,83 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+    static void Pattern1(int n ) {
+        /*
+         * *
+         * * *
+         * * * *
+         * * * * *
+         */
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    static void Pattern2(int n) {
+        /*
+         * 1
+         * 1 2
+         * 1 2 3
+         * 1 2 3 4
+         * 1 2 3 4 5
+         */
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
+
+    static void Pattern3(int n) {
+        /*
+         * 1
+         * 2 2
+         * 3 3 3
+         * 4 4 4 4
+         * 5 5 5 5 5
+         */
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.println(" ");
+        }
+    }
+
+    static void Pattern4(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 0; j < n - i + 1; j++) {
+                System.err.print("*");
+            }
+            System.out.println(" ");
+        }
+    }
+
+    static void Pattern5(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println(" ");
+        }
+    }
+
+    static void Pattern6(int n){
+        for(int i=1; i<=n;i++){
+
+        }
+    }
+
+    public static void main(String[] args) {
+
+        // Pattern1(5);
+        // Pattern2(5);
+        // Pattern3(5);
+        // Pattern4(5);
+        Pattern5(5);
     }
 }
--- 05.Patterns/src/FourStars.java
+++ 05.Patterns/src/FourStars.java
@@ -2,9 +2,9 @@ public class FourStars{
     public static void main(String[] args) {
         for(int i=0; i<4; i++){
             for(int j=0;j<4;j++){
-                System.out.println("*");
+                System.out.print("*");
             }
-            System.err.println();
+            System.err.print(" ");
         } 
     }
 }
