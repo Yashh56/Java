81e94783ffda23a391651d6a2d84aea91c1c4203
yashh56 <yashsaini18166@gmail.com>
2024-01-23 18:48:20 +0530

    recursion updated

--- Recursion/src/ProblemsOnRecursion.java
+++ Recursion/src/ProblemsOnRecursion.java
@@ -27,36 +27,83 @@ public class ProblemsOnRecursion {
         linear(n, i + 1);
     }
 
-    static void oppLinear(int n,int i){
+    static void oppLinear(int n, int i) {
         // Printing n to 1
-        if(i<1) return;
+        if (i < 1)
+            return;
         System.out.println(i);
-        oppLinear(n, i-1);
+        oppLinear(n, i - 1);
     }
-    
-    static void BLinear(int n,int i){
+
+    static void BLinear(int n, int i) {
         // Printing 1 to n using Backtracking
 
-        if(i<1) return;
-        BLinear(n, i-1);
+        if (i < 1)
+            return;
+        BLinear(n, i - 1);
         System.out.println(i);
     }
 
-    // static void BLinearOpp(int n , int i){
-    //     if(i<1) return ;
-    //     BLinearOpp(n, i+1);
-    //     System.out.println(i);
-    // }
+   
+    static void Sum(int i, int n) {
+        // Parameterized Way
+        if (i < 1) {
+            System.out.println(n);
+            return;
+        }
+        Sum(i - 1, n + i);
+    }
 
+    static int Sum2(int n) {
+        // Functional Way
+        if (n == 0) {
+            return 0;
+        }
+        return n + Sum2(n - 1);
+    }
+
+    // Reverse an array using Recursion 
+    static void printArray(int n ,int ans[]){
+        System.out.println("The reversed array is /n :");
+        for(int i=0;i<n;i++){
+            System.out.println(ans[i]+" ");
+        }
+    }
+
+    static void reversedArray(int n , int arr[]){
+        int[] ans = new int[n];
+        for(int i =n-1;i>=0;i--){
+            ans[n-i-1]=arr[i];
+        }
+        printArray(n, ans);
+    }
+
+    static int Factorial(int n) {
+        // Factorial
+        if (n == 1) {
+            return 1;
+        }
+        return n * Factorial(n - 1);
+    }
+
+    static int Fibb(int n){
+        if(n==1 || n==2 ) return 1;
+        return Fibb(n-1)+Fibb(n-2);
+    }
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
+        // int arr[] ={1,2,3,4,5}; 
         // Recur(n, 1);
         // name(n, 1);
         // linear(n, 1);
         // oppLinear(n, n);
-        BLinear(n, n);
-        // BLinearOpp(n, n);
+        // BLinear(n, n);
+        // Sum(n, 0);
+        // System.out.println(Sum2(n));
+        // System.out.println(Factorial(n));
+        // reversedArray(n,arr);
+        System.out.println(Fibb(n));
         input.close();
     }
 }
