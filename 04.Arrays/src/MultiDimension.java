35cca41ad5efa7175fc70175d278ca8cf18df063
Yash <yashsaini18166@gmail.com>
2024-02-16 23:13:00 +0530

    Arrays Hard 04

--- 04.Arrays/src/MultiDimension.java
+++ 04.Arrays/src/MultiDimension.java
@@ -9,10 +9,20 @@ public class MultiDimension {
          */
 
         int[][] arr = {
-                { 1, 2, 3 },
-                { 4, 5, 6 },
-                { 7, 8, 9 }
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 1, 0, 0 },
+                { 0, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 }
         };
-        System.out.println(Arrays.toString(arr));
+        // System.out.println(Arrays.toString(arr));
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+
+                System.out.print(arr[i][j]);
+
+            }
+            System.out.println();
+        }
     }
 }
